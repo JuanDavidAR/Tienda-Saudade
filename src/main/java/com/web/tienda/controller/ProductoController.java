7ab25b0ef111b7d5008d43bc197481c9aa22360c
JuanDavidAR <aldana.juand@javeriana.edu.co>
2022-04-24 23:44:24 -0500

    Create ProductoController.java

---
+++ src/main/java/com/web/tienda/controller/ProductoController.java
@@ -0,0 +1,122 @@
+package com.web.tienda.controller;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.web.tienda.model.Producto;
+import com.web.tienda.model.Usuario;
+import com.web.tienda.service.ProductoService;
+import com.web.tienda.service.UploadFileService;
+
+@Controller
+@RequestMapping("/productos")
+public class ProductoController {
+
+	
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+	
+	@Autowired
+	private ProductoService productoService;
+	
+	@Autowired
+	private UploadFileService upload;
+	
+	
+	
+	@GetMapping("")
+	public String show(Model model) {
+		model.addAttribute("productos", productoService.findAll());
+		return "productos/show";
+	}
+	
+	@GetMapping("/create")
+	public String create () {
+		return"productos/create";
+	}
+	
+	@PostMapping("/save")
+	public String save(Producto producto, @RequestParam("img") MultipartFile file) throws IOException
+	{
+		LOGGER.info("Este es el objeto producto {}",producto);
+		Usuario u = new Usuario (1,"","","","","","","");
+		producto.setUsuario(u);
+		
+		//imagen 
+		if(producto.getId()==null) {//cuando se crea un producto
+			
+			String nombreImagen = upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+		}else {
+			
+		}
+		
+		
+		productoService.save(producto);
+		return "redirect:/productos";
+		
+	}	
+	
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Integer id, Model model) {
+		
+		Producto producto = new Producto();
+		Optional<Producto> optionalProducto = productoService.get(id);
+		producto= optionalProducto.get();
+		
+		LOGGER.info("Producto buscado: {}",producto);
+		model.addAttribute("producto", producto);
+		return "productos/edit";	
+	}
+	
+	@PostMapping("/update")
+	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+		if(file.isEmpty()) {
+			Producto p= new Producto();
+			p=productoService.get(producto.getId()).get();
+			producto.setImagen(p.getImagen());
+		}else { //cuando se edita la imagen 
+			
+			Producto p = new Producto();
+			p=productoService.get(producto.getId()).get();
+			
+			if(!p.getImagen().equals("default.jpg")) {
+				upload.deleteImage(p.getImagen());	
+			}
+
+			String nombreImagen = upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+		}
+		
+		productoService.update(producto);
+		return"redirect:/productos";
+	}
+	
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Integer id) {
+		
+		Producto p = new Producto();
+		p=productoService.get(id).get();
+		
+		if(!p.getImagen().equals("default.jpg")) {
+			upload.deleteImage(p.getImagen());
+			
+		}
+		
+		
+		productoService.delete(id);
+		return "redirect:/productos";
+	}
+	
+}

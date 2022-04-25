2bf40a09d8d6f6207d047a20a884f0dc09e04fb8
JuanDavidAR <aldana.juand@javeriana.edu.co>
2022-04-24 23:44:26 -0500

    Create AdministradorController.java

---
+++ src/main/java/com/web/tienda/controller/AdministradorController.java
@@ -0,0 +1,18 @@
+package com.web.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/administrador")
+public class AdministradorController {
+	
+	@GetMapping("")
+	public String home(){
+		
+		return "administrador/home";
+	}
+	
+
+}

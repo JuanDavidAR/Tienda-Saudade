3cf4d7114d3f5db9d58b25d36ad87746f17d5385
Johanna Bolívar <71041573+Johabc@users.noreply.github.com>
2022-04-25 21:08:54 -0500

    .

--- src/main/java/com/web/tienda/controller/AdministradorController.java
+++ src/main/java/com/web/tienda/controller/AdministradorController.java
@@ -1,15 +1,28 @@
 package com.web.tienda.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.web.tienda.model.Producto;
+import com.web.tienda.service.ProductoService;
 
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorController {
 	
+	@Autowired
+	private ProductoService productoService;  
+	
 	@GetMapping("")
-	public String home(){
+	public String home(Model model){
+		
+		List<Producto> productos = productoService.findAll();
+		model.addAttribute("productos", productos); 
 		
 		return "administrador/home";
 	}
--- src/main/java/com/web/tienda/controller/ProductoController.java
+++ src/main/java/com/web/tienda/controller/ProductoController.java
@@ -81,14 +81,13 @@ public class ProductoController {
 	
 	@PostMapping("/update")
 	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+		Producto p= new Producto();
+		p=productoService.get(producto.getId()).get();
+		
 		if(file.isEmpty()) {
-			Producto p= new Producto();
-			p=productoService.get(producto.getId()).get();
+			
 			producto.setImagen(p.getImagen());
 		}else { //cuando se edita la imagen 
-			
-			Producto p = new Producto();
-			p=productoService.get(producto.getId()).get();
 			
 			if(!p.getImagen().equals("default.jpg")) {
 				upload.deleteImage(p.getImagen());	
@@ -97,7 +96,7 @@ public class ProductoController {
 			String nombreImagen = upload.saveImage(file);
 			producto.setImagen(nombreImagen);
 		}
-		
+		producto.setUsuario(p.getUsuario());
 		productoService.update(producto);
 		return"redirect:/productos";
 	}
